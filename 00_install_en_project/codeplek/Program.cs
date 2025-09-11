6dd8d6f15902cfe54c19e9ddb05c52575420b054
Niklas Nikolaidis <Niklasn1809@icloud.com>
2025-09-11 13:06:45 +0200

    m1prog initial commit

---
+++ 00_install_en_project/codeplek/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace codeplek;
+
+//Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, User1235!");
+    }
+}
---
+++ 00_install_en_project/verhaal/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace verhaal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello and Welkom to videogame Quotes!                               ");
+        Console.WriteLine("                                                                    ");
+        Console.WriteLine("____________________________________________________________________");
+        Console.WriteLine("Nr. 1                                                               ");
+        Console.WriteLine("Did I ever tell you what the definition of insanity is? - Far Cry 3 ");
+        Console.WriteLine("                                                                    ");
+        Console.WriteLine("Nr. 2                                                               ");
+        Console.WriteLine("War. War never changes. - Fallout series                            ");
+        Console.WriteLine("                                                                    ");
+        Console.WriteLine("Nr. 3                                                               ");
+        Console.WriteLine("It's dangerous to go alone! Take this. - The Legend of Zelda        ");
+        Console.WriteLine("____________________________________________________________________");
+
+
+
+    }
+}
