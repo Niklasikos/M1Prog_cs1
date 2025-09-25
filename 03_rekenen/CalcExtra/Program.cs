3c1b19b7d21a7c9f5acfb8746175ecdb6ca0a36b
Niklasikos <40353@ma-web.nl>
2025-09-25 11:51:33 +0200

    extra opdracht calcextra af

---
+++ 03_rekenen/CalcExtra/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CalcExtra;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float x1 = 50;
+        float y1 = 50;
+        float x2 = 60;
+        float y2 = 50;
+
+        float rad1 = 5;
+        float rad2 = 5;
+
+        float dx = x2 - x1;
+        float dy = y2 - y1;
+
+        float testLengh = 5;
+
+        double length = Math.Sqrt((dx * dx) + (dy * dy));
+
+        bool collide = length <= testLengh;
+
+        Console.WriteLine(collide);
+    }
+}
---
+++ 04_beslissen/als/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Program;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool sleutelKwijt = false;
+        Console.WriteLine("Je zoekt je sleutel");
+        if (sleutelKwijt == true)
+        {
+            Console.WriteLine("Je bent je sleutel kwijt!");
+            Console.WriteLine("de deur blijft gesloten");
+        }
+        else
+        {
+
+            
+        }
+    }
+}
